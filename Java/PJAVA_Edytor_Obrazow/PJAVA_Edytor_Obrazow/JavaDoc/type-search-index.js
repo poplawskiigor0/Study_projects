fce6d8f246413ac881f8785443882d3f5adabb16
Igor Popławski <01178500@pw.edu.pl>
2025-03-09 18:17:58 +0100

    add

--- Java/PJAVA_Edytor_Obrazow/PJAVA_Edytor_Obrazow/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Buttons"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"ImageDisplay"},{"p":"<Unnamed>","l":"ImageModifier"},{"p":"<Unnamed>","l":"ImagePanel"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
